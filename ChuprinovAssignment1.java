815784b1e1ba1feb5016b97e6f5b96acab815915
ivanchuprinov <chuprii@wwu.edu>
2018-10-10 11:50:07 -0700

    Started working on splits

--- ChuprinovAssignment1.java
+++ ChuprinovAssignment1.java
@@ -23,10 +23,15 @@ public class ChuprinovAssignment1
 				{
 					System.out.println("Crazy day: " + crazies.get(i).get(j));
 				}
-				System.out.println();
-				System.out.println("The craziest day is: " + getCraziest(crazies.get(i)));
+				System.out.println("Total crazy days = " + (crazies.get(i).size() - 1));
+				if(crazies.get(i).size() - 1 > 1)
+					System.out.println("The craziest day: " + getCraziest(crazies.get(i)));
 				System.out.println();
 			}
+
+
+
+			scanIn.close();
 		}
 		catch(FileNotFoundException e)
 		{
@@ -42,40 +47,36 @@ public class ChuprinovAssignment1
 	 * of companies into ArrayLists which are all stored in one big ArrayList. The first entry
 	 * of a company's list is always the company's ticker symbol.
 	 */
-
-	/* TODO
-		-Add companies even if they don't have crazy days by fiddling with the if statements
-	*/
-
 	public static ArrayList<ArrayList<String>> getCrazies(Scanner sc)
 	{
 		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
 		HashMap<String, Integer> indexMap = new HashMap<String, Integer>();
 		int companyIndex = 0;
+		String candidate;
+		String[] candArr;
+		double highPrice;
+		double lowPrice;
+		double percentage;
 
 		while(sc.hasNext())
 		{
-			String candidate = sc.nextLine();
-			String[] candArr = candidate.split("\t");
+			candidate = sc.nextLine();
+			candArr = candidate.split("\t");
 
-			double highPrice = Double.valueOf(candArr[3]);
-			double lowPrice = Double.valueOf(candArr[4]);
-			double percentage = (highPrice - lowPrice) / highPrice * 100;
+			highPrice = Double.valueOf(candArr[3]);
+			lowPrice = Double.valueOf(candArr[4]);
+			percentage = (highPrice - lowPrice) / highPrice * 100;
 
+			if(!indexMap.containsKey(candArr[0]))
+			{
+				indexMap.put(candArr[0], companyIndex);
+				companyIndex++;
+				result.add(new ArrayList<String>());
+				result.get(indexMap.get(candArr[0])).add(candArr[0]);
+			}
 			if(percentage >= CRAZY_PERCENTAGE)
 			{
-				if(indexMap.containsKey(candArr[0]))
-				{
-					result.get(indexMap.get(candArr[0])).add(String.format("%s\t%.2f", candArr[1], percentage));
-				}
-				else
-				{
-					indexMap.put(candArr[0], companyIndex);
-					companyIndex++;
-					result.add(new ArrayList<String>());
-					result.get(indexMap.get(candArr[0])).add(candArr[0]);
-					result.get(indexMap.get(candArr[0])).add(String.format("%s\t%.2f", candArr[1], percentage));
-				}
+				result.get(indexMap.get(candArr[0])).add(String.format("%s\t%.2f", candArr[1], percentage));
 			}
 		}
 
@@ -101,6 +102,40 @@ public class ChuprinovAssignment1
 		}
 
 		return crazyList.get(craziestIndex);
+	}
+
+	public static ArrayList<ArrayList<String>> getSplits(Scanner sc)
+	{
+
+		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
+		if(sc.hasNext()){
+			String newDay = sc.nextLine();
+			String oldDay;
+			String[] newArr;
+			String[] oldArr;
+
+			HashMap<String, Integer> indexMap = new HashMap<String, Integer>();
+			int companyIndex = 0;
+
+			while(sc.hasNext())
+			{
+				newDay = sc.nextLine();
+				oldDay = newDay;
+				newArr = newDay.split("\t");
+				oldArr = oldDay.split("\t");
+				double oldClose = Double.valueOf(oldArr[3]);
+				double newOpen = Double.valueOf(candArr[4]);
+				double percentage = (highPrice - lowPrice) / highPrice * 100;
+			}
+
+			sc.reset();
+		}
+		return result;
+	}
+
+	private static int splitType(double diff)
+	{
+
 	}
 
 }
