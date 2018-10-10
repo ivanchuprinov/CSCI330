01ca25eff384c501e619fd2400b433e2535ca814
ivanchuprinov <chuprii@wwu.edu>
2018-10-10 15:08:11 -0700

    Everything is broken

--- ChuprinovAssignment1.java
+++ ChuprinovAssignment1.java
@@ -5,15 +5,15 @@ public class ChuprinovAssignment1
 {
 
 	final static double CRAZY_PERCENTAGE = 15;
+	final static double SPLIT_VARIATION = 0.05;
+	final static String FILE_NAME = "./Stockmarket-1990-2015.txt";
 
 	public static void main(String[] args)
 	{
 		
 		try{
-			File input = new File("./Stockmarket-1990-2015.txt");
-			Scanner scanIn = new Scanner(input);
-
-			ArrayList<ArrayList<String>> crazies = getCrazies(scanIn);
+			ArrayList<ArrayList<String>> crazies = getCrazies(new Scanner(new File(FILE_NAME)));
+			ArrayList<ArrayList<String>> splits = getSplits(new Scanner(new File(FILE_NAME)));
 
 			for(int i = 0; i < crazies.size(); i++)
 			{
@@ -27,11 +27,12 @@ public class ChuprinovAssignment1
 				if(crazies.get(i).size() - 1 > 1)
 					System.out.println("The craziest day: " + getCraziest(crazies.get(i)));
 				System.out.println();
+				for(int j = 0; j < splits.get(i).size(); j++)
+				{
+					System.out.println(splits.get(i).get(j));
+				}
 			}
 
-
-
-			scanIn.close();
 		}
 		catch(FileNotFoundException e)
 		{
@@ -58,7 +59,7 @@ public class ChuprinovAssignment1
 		double lowPrice;
 		double percentage;
 
-		while(sc.hasNext())
+		while(sc.hasNextLine())
 		{
 			candidate = sc.nextLine();
 			candArr = candidate.split("\t");
@@ -80,7 +81,7 @@ public class ChuprinovAssignment1
 			}
 		}
 
-		sc.reset();
+		sc.close();
 
 		return result;
 	}
@@ -106,36 +107,69 @@ public class ChuprinovAssignment1
 
 	public static ArrayList<ArrayList<String>> getSplits(Scanner sc)
 	{
-
+		
+		
 		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>>();
-		if(sc.hasNext()){
+		if(sc.hasNextLine()){
+			int companyIndex = 0;
+			double oldClose = 0;
+			double newOpen = 0;
+			double ratio = 0;
 			String newDay = sc.nextLine();
-			String oldDay;
-			String[] newArr;
-			String[] oldArr;
+			String oldDay = null;
+			String[] newArr = null;
+			String[] oldArr = null;
+			String splType = null;
 
 			HashMap<String, Integer> indexMap = new HashMap<String, Integer>();
-			int companyIndex = 0;
 
-			while(sc.hasNext())
-			{
-				newDay = sc.nextLine();
-				oldDay = newDay;
-				newArr = newDay.split("\t");
+			while(sc.hasNextLine())
+			{				
+
+				newDay = oldDay;
+				oldDay = sc.nextLine();				
 				oldArr = oldDay.split("\t");
-				double oldClose = Double.valueOf(oldArr[3]);
-				double newOpen = Double.valueOf(candArr[4]);
-				double percentage = (highPrice - lowPrice) / highPrice * 100;
+				newArr = newDay.split("\t");
+
+				if(oldArr[0].equals(newArr[0]))
+				{
+					oldClose = Double.valueOf(oldArr[5]);
+					newOpen = Double.valueOf(newArr[2]);
+					ratio = oldClose/newOpen;
+					System.out.println(oldClose + " / " + newOpen + " = " + ratio);
+					if(!indexMap.containsKey(oldArr[0]))
+					{
+						indexMap.put(oldArr[0], companyIndex);
+						companyIndex++;
+						result.add(new ArrayList<String>());
+						result.get(indexMap.get(oldArr[0])).add(oldArr[0]);
+					}
+					splType = splitType(ratio);
+					if(splType != null)
+						result.get(indexMap.get(oldArr[0])).add(String.format(
+							"%s split on %s\t%.2f --> %.2f", splType, oldArr[1], oldClose, newOpen));
+				}
 			}
 
-			sc.reset();
+			sc.close();
 		}
 		return result;
 	}
 
-	private static int splitType(double diff)
+	private static String splitType(double ratio)
 	{
-
+		if(Math.abs(ratio - 1.5) < SPLIT_VARIATION || ratio > 1.5)
+		{
+			if (Math.abs(ratio - 2) < SPLIT_VARIATION || ratio > 2)
+			{
+				if (Math.abs(ratio - 3) < SPLIT_VARIATION || ratio > 3)
+					return "3:1";
+				return "2:1";
+			}
+			return "3:2";
+		}
+		else
+			return null;
 	}
 
 }
